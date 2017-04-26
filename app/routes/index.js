e71adcf8ab86d0ef410fb5b9f957068c990879f0
evan bishop and liam stabeno <liam@>
2017-04-26 16:19:06 -0700

    succesffuly return information for single rentals

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,31 +1,13 @@
 import Ember from 'ember';
 
-
-
 export default Ember.Route.extend({
-  model: function() {
-    return this.store.findAll('rental');
-  },
-
-  actions: {
+  model() {
+     return this.store.findAll('rental');
+   },
+   actions: {
     saveRental3(params) {
       var newRental = this.store.createRecord('rental', params);
       newRental.save();
-      this.transitionTo('index');
-    },
-
-    update(rental, params) {
-      Object.keys(params).forEach(function(key)  {
-        if(params[key]!==undefined) {
-          rental.set(key, params[key]);
-        }
-      });
-      rental.save();
-      this.transitionTo('index');
-    },
-
-    destroyRental(rental)  {
-      rental.destroyRecord();
       this.transitionTo('index');
     }
   }
